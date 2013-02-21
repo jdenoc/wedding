18fec831ae5f4cc608c48f0bc37e3898a21cd920
jdenoc <jdenoc@gmail.com>
2013-02-20 21:05:11 -0600

    Version 3.1.3
    Corrected minor problem with gallery page.
    css was not added.

--- gallery/res/header_details.php
+++ gallery/res/header_details.php
@@ -1,3 +1,4 @@
 <title>Denis & Britain's Wedding Reception</title>
 <link rel="icon" href="../imgs/favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="Slideshow/style.css" />
+<link rel="stylesheet" href="../css/wedding-css.css" type="text/css"/>
